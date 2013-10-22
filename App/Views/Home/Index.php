5531e80912d2753b3492bdb3138c24a369979e53
michaelvivar <michaelvictorvivar@gmail.com>
2013-10-22 09:45:32 +0800

    Create Index.php

---
+++ App/Views/Home/Index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html lang="en">
+	<head>
+		<title>MVC</title>
+	</head>
+	<body>
+		<?php
+			$this->form->label('email');
+			$this->form->input('email');
+			$this->form->label('Contry');
+			$this->form->select('country');
+		?>
+	</body>
+</html>
