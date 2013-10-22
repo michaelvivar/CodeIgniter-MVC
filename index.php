6edb62586d9ae1f45b382f480a6b07d9b4180944
michaelvivar <michaelvictorvivar@gmail.com>
2013-10-22 09:33:49 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+define('BASEPATH', __DIR__ . '/');
+
+function autoload($className)
+{
+	$className = ltrim($className, '\\');
+	$fileName = '';
+	$namespace = '';
+	if ($lastNsPos = strrpos($className, '\\'))
+	{
+		$namespace = substr($className, 0, $lastNsPos);
+		$className = substr($className, $lastNsPos + 1);
+		$fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+	}
+	$fileName .= str_replace('_', DIRECTORY_SEPARATOR, $className) . '.php';
+	if (file_exists($fileName))
+	{
+		include_once $fileName;
+	}
+}
+
+spl_autoload_register('autoload');
+
+new Core\Request();
