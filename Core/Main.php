a3f61c62bd738602f4dd8459422b8f19eb26dc79
michaelvivar <michaelvictorvivar@gmail.com>
2013-10-29 14:38:02 +0800

    Update Main.php

--- Core/Main.php
+++ Core/Main.php
@@ -6,7 +6,7 @@ class Main
 {
 	function __construct($routes)
 	{
-		$class = '\\App\\Controllers\\' . $routes->controller;
+		$class = '\\App\\Controllers\\' . $routes->controller . 'Controller';
 		call_user_func_array(array(new $class, $routes->action), $routes->params);
 	}
 }
